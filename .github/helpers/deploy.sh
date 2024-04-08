b4691828d5942834838b9b3efcf6b074f3774f00
Adam Kroon <Adam.Kroon@gov.bc.ca>
2024-04-08 09:59:21 -0700

    AO-910: troubleshooting deploy

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -3,7 +3,7 @@
 # Use DeploymentConfigs to deploy the application to OpenShift.
 
 
-  oc process -f /home/runner/work/citz-imb-saas-integrations/citz-imb-saas-integrations/openshift/templates/"$DC_TEMPLATE" --namespace=$NAMESPACE \
+  oc process -f /home/runner/work/citz-imb-saas-integrations/citz-imb-saas-integrations/openshift/templates/miro-privacy-popup/"$DC_TEMPLATE" --namespace=$NAMESPACE \
       -p LICENSE_PLATE="$LICENSE_PLATE" \
       -p IMAGE_TAG=$IMAGE_TAG \
       -p ENVIRONMENT=$ENVIRONMENT | \
